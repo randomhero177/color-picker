b49cba8660e84c0bcdab13f088813672d72f0935
Konstantin Vasin <randomhero177@gmail.com>
2022-12-09 02:07:56 +0300

    add hash

--- app.js
+++ app.js
@@ -1,54 +1,51 @@
-console.log('working');
-
-
 const cols = document.querySelectorAll('.col');
 
 document.addEventListener('keydown', event => {
     if (event.code.toLowerCase() === 'space') {
+        event.preventDefault();
         setRandomColors();
     }
 })
 
 document.addEventListener('click', (event) => {
     const type = event.target.dataset.type;
-
+    const node = event.target.tagName.toLowerCase() === 'i'
+        ? event.target
+        : event.target.children[0];
+    const button = event.target.tagName.toLowerCase() === 'i'
+        ? event.target.parentNode
+        : event.target;
     if (type === 'lock') {
-        const node = event.target.tagName.toLowerCase() === 'i'
-            ? event.target
-            : event.target.children[0];
-        const button = event.target.tagName.toLowerCase() === 'i'
-            ? event.target.parentNode
-            : event.target;
-        console.log(event);
-        console.log(button);
-        console.log('lock');
         node.classList = 'fa-solid fa-lock';
         node.dataset.type = button.dataset.type = 'open';
     } else if(type === 'open') {
-        console.log('open')
+        node.classList = 'fa-solid fa-lock-open';
+        node.dataset.type = button.dataset.type = 'lock';
     } else if (type === 'copy') {
-        console.log('open')
         copyToClipboard(event.target.textContent);
     }
 })
 
-function setRandomColors() {
-    cols.forEach((item) => {
-        console.log(item);
+function setRandomColors(hashedColors = []) {
+    let colorsArr = '';
+    cols.forEach((item, i) => {
         const isLocked = item.querySelector('i').classList.contains('fa-lock');
+        const color = hashedColors[i] || generateRandomColors();
+        const h2 = item.querySelector('h2');
+        colorsArr += `${isLocked ? h2.textContent.substring(1) : color.substring(1)}${cols.length - 1 > i ? '-' : ''}`;
+
         if (isLocked) return
 
-        const color = generateRandomColors();
-        const h2 = item.querySelector('h2');
         const button = item.querySelector('button');
         h2.textContent = color;
         setTextColor(h2, color);
         setTextColor(button, color);
         item.style.background = color;
     });
+    window.location.hash = colorsArr;
 }
 
-setRandomColors();
+
 
 function generateRandomColors() {
     const hexCodes = '0123456789ABCDEF';
@@ -68,3 +65,11 @@ function setTextColor(elem, color) {
 function copyToClipboard(text) {
     return navigator.clipboard.writeText(text);
 }
+
+function getColorsFromHash() {
+    const hash = window.location.hash;
+    const data = hash ? hash.substring(1).split('-').map(color => `#${color}`) : [];
+    setRandomColors(data);
+}
+
+getColorsFromHash();
